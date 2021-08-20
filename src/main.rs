14cd5aeefd661e11221f73eb374434a0c8169c1a
Nyelsonon <nyelsonexists@gmail.com>
2021-08-20 19:04:39 -0400

    windows changes

--- src/main.rs
+++ src/main.rs
@@ -35,26 +35,51 @@ fn main() {
 
 
 pub fn ascii() {
-    let mut sys = sysinfo::System::new();
-    sys.refresh_all();
-    let num = num_cpus::get();
-    println!("{}", "      ______________".red().bold());
-    println!("{}        {}", "     /_____________/|".red().bold(), "<-----System Info----->".blue().bold());
-    println!("{}", "    /_____________/ |".yellow().bold());
-    println!("{}        {} {:?} KB", "   /____________ /  |".yellow().bold(), "Total Memory:".bold().cyan(), sys.total_memory());
-    println!("{}        {} {:?} KB", "  | ___________ |   |".green().bold(), "Used Memory:".bold().cyan(), sys.used_memory());
-    println!("{}{}{}        {} {:?} KB", "  ||     ".green().bold(),"/","     ||   |".green().bold(), "Total Swap:".bold().cyan(), sys.total_swap());
-    println!("{}{}{}        {} {:?} KB", "  ||  ".blue().bold(),"/||||\\","   ||   |".blue().bold(), "Used Swap:".bold().cyan(),sys.used_swap());
-    println!("{}{}{}        {} {:?}", "  ||  ".blue().bold(),"\\||||/","   ||   |".blue().bold(), "Name:".bold().cyan(),sys.name());
-    println!("{}        {} {:?}", "  ||___________||   |".cyan().bold(), "Kernel Version:".bold().cyan(),sys.kernel_version());
-    println!("{}        {} {:?}", "  ||           ||   |".cyan().bold(), "OS Version:".bold().cyan(),sys.os_version());
-    println!("{}         {} {:?}", "  |   _______   |  /".purple().bold(), "Host Name:".bold().cyan(),sys.host_name());
-    println!("{}         {} {:?}", "  /|  (_______)  | /".purple().bold(), "CPU Core Count:".bold().cyan(), num);
-    println!("{}", " (  |_____________|/".red().bold());
-    println!("{}        {}", "  \\".red().bold(), "                  <-----System Info----->".blue().bold());
-    println!("{}", ".=======================.".black().bold());
-    println!("{}", "| ::::::::::::::::  ::: |".black().bold());
-    println!("{}", "| ::::::::::::::[]  ::: |".black().bold());
-    println!("{}", "|   -----------     ::: |".black().bold());
-    println!("{}", "`-----------------------'".black().bold());
+    if cfg!(target_os="macos"){
+        let mut sys = sysinfo::System::new();
+        sys.refresh_all();
+        let num = num_cpus::get();
+        println!("{}", "      ______________".red().bold());
+        println!("{}        {}", "     /_____________/|".red().bold(), "<-----System Info----->".blue().bold());
+        println!("{}", "    /_____________/ |".yellow().bold());
+        println!("{}        {} {:?} KB", "   /____________ /  |".yellow().bold(), "Total Memory:".bold().cyan(), sys.total_memory());
+        println!("{}        {} {:?} KB", "  | ___________ |   |".green().bold(), "Used Memory:".bold().cyan(), sys.used_memory());
+        println!("{}{}{}        {} {:?} KB", "  ||     ".green().bold(),"/","     ||   |".green().bold(), "Total Swap:".bold().cyan(), sys.total_swap());
+        println!("{}{}{}        {} {:?} KB", "  ||  ".blue().bold(),"/||||\\","   ||   |".blue().bold(), "Used Swap:".bold().cyan(),sys.used_swap());
+        println!("{}{}{}        {} {:?}", "  ||  ".blue().bold(),"\\||||/","   ||   |".blue().bold(), "Name:".bold().cyan(),sys.name());
+        println!("{}        {} {:?}", "  ||___________||   |".cyan().bold(), "Kernel Version:".bold().cyan(),sys.kernel_version());
+        println!("{}        {} {:?}", "  ||           ||   |".cyan().bold(), "OS Version:".bold().cyan(),sys.os_version());
+        println!("{}         {} {:?}", "  |   _______   |  /".purple().bold(), "Host Name:".bold().cyan(),sys.host_name());
+        println!("{}         {} {:?}", "  /|  (_______)  | /".purple().bold(), "CPU Core Count:".bold().cyan(), num);
+        println!("{}", " (  |_____________|/".red().bold());
+        println!("{}        {}", "  \\".red().bold(), "                  <-----System Info----->".blue().bold());
+        println!("{}", ".=======================.".black().bold());
+        println!("{}", "| ::::::::::::::::  ::: |".black().bold());
+        println!("{}", "| ::::::::::::::[]  ::: |".black().bold());
+        println!("{}", "|   -----------     ::: |".black().bold());
+        println!("{}", "`-----------------------'".black().bold());
+    } else if cfg!(windows){
+        let mut sys = sysinfo::System::new();
+        sys.refresh_all();
+        let num = num_cpus::get();
+        println!("{}", "      ______________".red().bold());
+        println!("{}        {}", "     /_____________/|".red().bold(), "<-----System Info----->".blue().bold());
+        println!("{}", "    /_____________/ |".yellow().bold());
+        println!("{}        {} {:?} KB", "   /____________ /  |".yellow().bold(), "Total Memory:".bold().cyan(), sys.total_memory());
+        println!("{}        {} {:?} KB", "  | ___________ |   |".green().bold(), "Used Memory:".bold().cyan(), sys.used_memory());
+        println!("{}{}{}        {} {:?} KB", "  ||     ".green().bold(),"","     ||   |".green().bold(), "Total Swap:".bold().cyan(), sys.total_swap());
+        println!("{}{}{}        {} {:?} KB", "  ||  ".blue().bold(),"|_| |_|","   ||   |".blue().bold(), "Used Swap:".bold().cyan(),sys.used_swap());
+        println!("{}{}{}        {} {:?}", "  ||  ".blue().bold(),"|_| |_|","   ||   |".blue().bold(), "Name:".bold().cyan(),sys.name());
+        println!("{}        {} {:?}", "  ||___________||   |".cyan().bold(), "Kernel Version:".bold().cyan(),sys.kernel_version());
+        println!("{}        {} {:?}", "  ||           ||   |".cyan().bold(), "OS Version:".bold().cyan(),sys.os_version());
+        println!("{}         {} {:?}", "  |   _______   |  /".purple().bold(), "Host Name:".bold().cyan(),sys.host_name());
+        println!("{}         {} {:?}", "  /|  (_______)  | /".purple().bold(), "CPU Core Count:".bold().cyan(), num);
+        println!("{}", " (  |_____________|/".red().bold());
+        println!("{}        {}", "  \\".red().bold(), "                  <-----System Info----->".blue().bold());
+        println!("{}", ".=======================.".black().bold());
+        println!("{}", "| ::::::::::::::::  ::: |".black().bold());
+        println!("{}", "| ::::::::::::::[]  ::: |".black().bold());
+        println!("{}", "|   -----------     ::: |".black().bold());
+        println!("{}", "`-----------------------'".black().bold());
+    }
 }
